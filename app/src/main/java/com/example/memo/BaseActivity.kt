e58f475c7a6dd7096b2fda4dccb46c4cfe59c4cd
chene <https://github.com/dr-chene>
2020-11-19 11:48:44 +0800

    first commit:add toolbar, recyclerview...

---
+++ app/src/main/java/com/example/memo/BaseActivity.kt
@@ -0,0 +1,37 @@
+package com.example.memo
+
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+Created by chene on @date 20-11-18 下午4:09
+ **/
+open class BaseActivity : AppCompatActivity() {
+
+    protected fun makeStatusBarTransparent(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            val option: Int = window.decorView
+                .systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.decorView.systemUiVisibility = option
+            window.statusBarColor = Color.TRANSPARENT
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
+    }
+
+    protected fun makeStatusBarIconDark(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val option = window.decorView.systemUiVisibility
+            window.decorView.systemUiVisibility = option or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = Color.parseColor("#80EEEEEE")
+        }
+    }
+}
--- app/src/main/java/com/example/memo/MainActivity.kt
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.memo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.memo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        makeStatusBarTransparent()
+        makeStatusBarIconDark()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        setSupportActionBar(binding.activityMainToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        when(item.itemId){
+
+        }
+        return true
     }
 }
